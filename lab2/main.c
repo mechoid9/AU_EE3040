a156ca8a35b89886dbbae70c74ab3e2fede8b7c4
mechoid9 <mechoid9@hotmail.com>
2011-08-30 02:50:31 -0500

    added header to main.c

---
+++ lab2/main.c
@@ -0,0 +1,8 @@
+/*-----------------------------------*/
+/*  Brian Arnberg and Stephen Taylor */
+/*  ELEC 3040 - Lab 2, main.c        */
+/*  Decade up/down counter,          */
+/*   count only when S1=1            */
+/*   count up when S2=0,             */
+/*   count down when S2=1	     */
+/*-----------------------------------*/
