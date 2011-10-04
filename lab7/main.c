d45581e88153580a1634c98fa71550fed38f5090
mechoid9 <mechoid9@hotmail.com>
2011-10-04 15:01:01 -0500

    updated main.c for PTT stuff

--- lab7/main.c
+++ lab7/main.c
@@ -1,99 +1,91 @@
 /*-------------------------------------*
  * Brian Arnberg and Stephen Taylor    
- * ELEC 3040 - Lab 6, main.c
- * Stop Watch - Decade 0.0 - 9.9 sec       
- *  always count up
- *  Keypad 1 - Start/Stop
- *  Keypad 0 - Reset
- *  Output Port AD
- *  Port T 7-4 - keypad columns, (output)
- *  Port T 3-0 - keypad rows, (input)
+ * ELEC 3040 - Lab 7a, main.c
+ * PWM Output Controller        
+ *  Keypad 1-9 - Set PWM speeds 1-9 
+ *  Keypad 0 - Stop, Speed to 0
+ *  Port AD 7-4 - keypad columns, (output)
+ *  Port AD 3-0 - keypad rows, (input)
+ *  Port T - PWM ouptut
  *  Port E 1 - IRQ           
  *-------------------------------------*/
 #include <hidef.h>      /* common defines and macros */
 #include <MC9S12C32.h>  /* derivative information */
 #pragma LINK_INFO DERIVATIVE "MC9S12C32"
+#define PERIOD 4000 /* A period of 1ms is 4000 cycles*/
 
 static unsigned char count1; //Count 1 - Left of the decimal      
 static unsigned char count2; //Count 2 - Right of the decimal             
 static unsigned char started; // 1 or 0; 1 = started, 0 = stopped
+static unsigned char high; //1 or 0; 1 = voltage high, 0 = low
+/* Duty Cycles */
+unsigned int duty[10]={0,400,800,1200,1600,2000,2400,2800,3200,3600}
 int i; // for "for" statements
 /*the keynumber is in hex, hex 0-D makes sense
  * hex E is * and hex F is # */
 static unsigned char keynumber;
+/* current command */
+unsigned char current;
+unsigned char previous; //previous timer value
 /* Define the IRQ service routine */
-
 interrupt void IRQ_ISR(void) {
-	/*	PTT = 0xF0;
-		for (i=0;i<4;i++) {
-		}; //small delay 
-		if (PTT_PTT7 == 0) { } 
-		else {*/
-		PTT = 0xE0; // PTT_PTT4 driven low, C1 low, Celse high
-		PTT = 0xE0; // PTT_PTT4 driven low, C1 low, Celse high
-		PTT = 0xE0; // PTT_PTT4 driven low, C1 low, Celse high
-		PTT = 0xE0; // PTT_PTT4 driven low, C1 low, Celse high
-		if (PTT_PTT0 == 0) {//row 1
+		PTAD = 0xE0; // PTAD_PTAD4 driven low, C1 low, Celse high
+			    for (i=0;i<4;i++) {
+		if (PTAD_PTAD0 == 0) {//row 1
 		keynumber = 0x01;//key 1
 		}
-		else if (PTT_PTT1 == 0){//row 2
+		else if (PTAD_PTAD1 == 0){//row 2
 		keynumber = 0x04;//key 4
 		}	
-		else if (PTT_PTT2 == 0){//row 3
+		else if (PTAD_PTAD2 == 0){//row 3
 		keynumber = 0x07;//key 7
 		}
-		else if (PTT_PTT3 == 0){//row 4
+		else if (PTAD_PTAD3 == 0){//row 4
 		keynumber = 0x0E;//key *
 		} else {
-		  	PTT = 0xD0; // PTT_PTT5 driven low, C2 low, Celse high
-		  	PTT = 0xD0; // PTT_PTT5 driven low, C2 low, Celse high
-		  	PTT = 0xD0; // PTT_PTT5 driven low, C2 low, Celse high
-		  	PTT = 0xD0; // PTT_PTT5 driven low, C2 low, Celse high
-		    if (PTT_PTT0 == 0) {//row 1
+		  	PTAD = 0xD0; // PTAD_PTAD5 driven low, C2 low, Celse high
+			    for (i=0;i<4;i++) {
+		    if (PTAD_PTAD0 == 0) {//row 1
 		    keynumber = 0x02;//key 2
 		    }
-		    else if (PTT_PTT1 == 0){//row 2
+		    else if (PTAD_PTAD1 == 0){//row 2
 		    keynumber = 0x05;//key 5
     	            }	
-		    else if (PTT_PTT2 == 0){//row 3
+		    else if (PTAD_PTAD2 == 0){//row 3
 		    keynumber = 0x08;//key 8
 		    }
-		    else if (PTT_PTT3 == 0){//row 4
+		    else if (PTAD_PTAD3 == 0){//row 4
 		    keynumber = 0x00;//key 0
 		    } else {
-		      PTT = 0xB0; // PTT_PTT6 driven low, C3 low, Celse high
-		      PTT = 0xB0; // PTT_PTT6 driven low, C3 low, Celse high
-		      PTT = 0xB0; // PTT_PTT6 driven low, C3 low, Celse high
-		      PTT = 0xB0; // PTT_PTT6 driven low, C3 low, Celse high
-		      if (PTT_PTT0 == 0) {//row 1
+		      PTAD = 0xB0; // PTAD_PTAD6 driven low, C3 low, Celse high
+			    for (i=0;i<4;i++) {
+		      if (PTAD_PTAD0 == 0) {//row 1
 		      keynumber = 0x03;//key 3
 		      }
-		      else if (PTT_PTT1 == 0){//row 2
+		      else if (PTAD_PTAD1 == 0){//row 2
 		      keynumber = 0x06;//key 6
 		      }	
-		      else if (PTT_PTT2 == 0){//row 3
+		      else if (PTAD_PTAD2 == 0){//row 3
 		      keynumber = 0x09;//key 9
 		      }
-		      else if (PTT_PTT3 == 0){//row 4
+		      else if (PTAD_PTAD3 == 0){//row 4
 		      keynumber = 0x0F;//key #
 		      } else {
-		        PTT = 0x70; // PTT_PTT7 driven low, C4 low, Celse high
-		        PTT = 0x70; // PTT_PTT7 driven low, C4 low, Celse high
-		        PTT = 0x70; // PTT_PTT7 driven low, C4 low, Celse high
-		        PTT = 0x70; // PTT_PTT7 driven low, C4 low, Celse high
-		        if (PTT_PTT0 == 0) {//row 1
+		        PTAD = 0x70; // PTAD_PTAD7 driven low, C4 low, Celse high
+			    for (i=0;i<4;i++) {
+		        if (PTAD_PTAD0 == 0) {//row 1
 		        keynumber = 0x0A;//key A
 		        }
-		        else if (PTT_PTT1 == 0){//row 2
+		        else if (PTAD_PTAD1 == 0){//row 2
 		        keynumber = 0x0B;//key B
 		        }	
-		        else if (PTT_PTT2 == 0){//row 3
+		        else if (PTAD_PTAD2 == 0){//row 3
 		        keynumber = 0x0C;//key C
 		        }
-		        else if (PTT_PTT3 == 0){//row 4
+		        else if (PTAD_PTAD3 == 0){//row 4
 		        keynumber = 0x0D;//key D
 		        } else { //No key found
-		        	PTT = 0x0F; //reset PTT columns to 0
+		        	PTAD = 0x0F; //reset PTAD columns to 0
 			        for (i=0;i<4;i++) {
 			        }; //small delay
 		          __asm rti //exit the interrupt because nothing happened
@@ -102,84 +94,81 @@ interrupt void IRQ_ISR(void) {
 		    }
 		  }
 		 
-		/* Test the key press, 
-		 * start or stop if the keypress is 0 */
-		if ( (keynumber == 0) && (started == 0) ) {//start
-			started = ~started; //toggle the "started" state
-			/* start the clock */
-			
-			TFLG1_C0F = 1; //reset timer interupt
-			TC0 = TCNT + 3125;//set new compare time
-			TIE_C0I = 1;//Timer Interupt Enable 0
-			PTT = 0x0F; //reset PTT columns to 0
-			for (i=0;i<4;i++) {
-			}; //small delay
-			__asm rti //use assembly code to exit the interupt
+	if ( (keynumber <= 9) && (keynumber >= 1) ) {//Set the current mode
+		if (!started) {//if not started, then initialize
+		TSCR1_TEN = 1;//enable timer
+		TIOS_IOS0 = 1;/*Set to output compare, Bit 0*/
+		TCTL2_OM5 = 0; /*Output Compair toggles PTT_PTT5 */
+		TCTL2_OL5 = 1;
+		TFLG1_C0F = 1; //reset timer interupt
+		TC0 = TCNT + PERIOD;//set new compare time
+		TIE_C0I = 1;//Timer Interupt Enable 0
+		started=~started;//toggle started
 		}
-		else if ( (keynumber == 0) && (started) ){//stop
-			started = ~started; //toggle the "started" state
-			/* stop the clock and stuff */
-			
-			TIE_C0I = 0; //disable timer interupts
-			PTT = 0x0F; //reset PTT columns to 0
-			for (i=0;i<4;i++) {
-			}; //small delay
-			__asm rti //use assembly code to exit the interupt
-	       	}
-		/* Else if the keypressed is 1 and the code is in 
-		 * the stopped state, then clear the display */
-		else if ( (keynumber == 1) && (started == 0) ) {
-			count1 = count2 = 0; //clear the counts to 0
-			PTAD = 0; //clear the LEDs
-			PTT = 0x0F; //reset PTT columns to 0
-			for (i=0;i<4;i++) {
-			}; //small delay
-			__asm rti //use assembly code to exit the interupt
-		}
-}
-
-
-/* redelay - increments the count
- * - executed by timer interupt
- * - outputs to Port AD */
-interrupt void TIMER_CHANNEL_0 (void) {
-	if ( count2 == 9 ) { 
-		count2 = 0; 
-		if ( count1 == 9 ) {
-			count1 = 0;
-			count2 = 0;
-		} else { count1 += 1; }
+		current = keynumber; //current mode
+		PTAD = 0x0F; //reset PTAD columns to 0
+		for (i=0;i<4;i++) {}; //small delay
+		__asm rti //use assembly code to exit the interupt
 	}
-	else { count2 += 1; }
-	TFLG1_C0F = 1; //reset timer interupt
-	TC0 += 3125;//set new compare time
-	TIE_C0I = 1;//Timer Interupt Enable 0
-	PTAD = count1*16 + count2; // output BCD to Port AD
+	else if (keynumber == 0) {//set current mode to stop
+		if (started) {//stop
+		current = keynumber; //current mode
+		TCTL2_OM5 = 1; /*Force PT5 to 0 */
+		TCTL2_OL5 = 0;
+		started=~started;//toggle started
+		}
+
+
 }
+
+
+/* TIMER_CHANNEL_0 - toggles PWM    
+ * - executed by timer interupt
+ * - No Output          */
+interrupt void TIMER_CHANNEL_0 (void) {
+	if (!high) { //turn on
+	high = ~high;//toggle high state
+	TFLG1_C0F = 1; //reset timer interupt
+	previous = TC0;//set the previous value
+	TC0 += duty[keynumber];//set new compare time(at the end of duty cycle)
+	TIE_C0I = 1;//Timer Interupt Enable 0
+	} else { //turn off
+	high = ~high;//toggle high state
+	TFLG1_C0F = 1; //reset timer interupt
+	TC0 = previous + PERIOD//set new compare time(beginning of next period)
+	TIE_C0I = 1;//Timer Interupt Enable 0
+	}
+}
+
 
 void main (void) {
 	DDRE = 0; //set Port E to read
-	DDRT = 0xF0; //set PortT bits 7-4 output, 3-0 input
-	DDRAD = 0xFF; //set PortAD to output
+	DDRT = 0xFF; //set PortT to output 
+	DDRAD = 0xF0; //set PortAD bits 7-4 output, 3-0 input
+	ATDDIEN = 0x0F; //Enable digital input buffer
 	
 	count1 = 0; //initialize count1 to 0 to start count up
 	count2 = 0; //initialize count2 to 0 to prevent countdown
 	started = 0; //initialize in the "unstarted" state
-	
-	PERT = 1; //Enable Port T's pull device
-	PPST = 0xF0; // Port T: 7-4 pull low; 3-0 pull up
-	PTT = 0x0F; //initialize Port T
-	
+	high = 0; //initialize high to 0
+	current = 1;//current mode is 
+
+	PERAD = 1; //Enable Port AD's pull device
+	PPSAD = 0xF0; // Port AD: 7-4 pull low; 3-0 pull up
+	PTAD = 0x0F; //initialize Port AD 
+/*	
 	TSCR1_TEN = 1;//enable timer
-	TSCR2_PR = 7;/*prescale factor (binary 111, or factor of 128 */
-	TIOS_IOS0 = 1;/*Set to output compare*/
+	//TSCR2_PR = 7; //prescale factor (binary 111, or factor of 128
+	TIOS_IOS0 = 1;//Set to output compare, Bit 0
+	TCTL2_OM5 = 0; //Output Compair toggles PTT_PTT5
+	TCTL2_OL5 = 1;
 	TC0 = TCNT;//Initialize TC0
-	
+*/	
 	INTCR_IRQEN = 1; /*enable IRQ# interrupts */
 	INTCR_IRQE = 1; /*IRQ# interrupts edge-triggered */
 	EnableInterrupts; /*clear I mask to enable interrupts */
 	//__asm ANDCC #0xBF /*clear X mast to enable XIRQ# */
 	while (1){
-		
+	__asm wai //Wait for interupt	
 	} /* repeat forever */
 }
